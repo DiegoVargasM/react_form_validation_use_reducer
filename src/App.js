c78461e675e14467df83a10f3ccf9ea85c4ae971
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-05-05 12:41:40 -0500

    format code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
   return (
     <div className="App">
-      <h1 className='text-center text-3xl font-semibold mt-2 py-4'>Registration Form</h1>
+      <h1 className="text-center text-3xl font-semibold mt-2 py-4">
+        Registration Form
+      </h1>
       <form>
-
         <div>
           <label htmlFor="firstName">First Name:</label>
-          <input type="text" name='firstName' />
+          <input
+            type="text"
+            name="firstName"
+            placeholder="John..."
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
         </div>
 
         <div>
           <label htmlFor="lastName">Last Name:</label>
-          <input type="text" name='lastName' />
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Doe..."
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
         </div>
 
         <div>
           <label htmlFor="email">Email:</label>
-          <input type="text" name='email' />
+          <input
+            type="text"
+            name="email"
+            placeholder="john@doe..."
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
-
       </form>
     </div>
   );
