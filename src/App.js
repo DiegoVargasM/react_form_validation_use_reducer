f68507c89157fffa62d79fb068d5e0d39ce59263
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-05-05 14:49:02 -0500

    Add comments

--- src/App.js
+++ src/App.js
@@ -1,49 +1,52 @@
 import "./App.css";
 import { useReducer } from "react";
 
-const initialState = {
-  firstName: "",
-  lastName: "",
-  email: "",
-  firstNameError: "",
-  lastNameError: "",
-  emailError: ""
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "SET_FIRST_NAME":
-      return {
-        ...state,
-        firstName: action.payload,
-        firstNameError: !/^[a-zA-Z]{3,12}$/.test(action.payload)
-          ? "First name must be 3-12 chars long."
-          : ""
-      };
-    case "SET_LAST_NAME":
-      return {
-        ...state,
-        lastName: action.payload,
-        lastNameError: !/^[a-zA-Z]{3,12}$/.test(action.payload)
-          ? "Last name must be 3-12 chars long."
-          : ""
-      };
-    case "SET_EMAIL":
-      return {
-        ...state,
-        email: action.payload,
-        emailError: !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{3,}$/.test(
-          action.payload
-        )
-          ? "Please enter a valid email"
-          : ""
-      };
-    default:
-      throw new Error();
-  }
-}
-
 function App() {
+
+  /* 1. definir objeto de estado inicial */
+  const initialState = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    firstNameError: "",
+    lastNameError: "",
+    emailError: ""
+  };
+  /* 2. definir funcion reductora */
+  function reducer(state, action) {
+    switch (action.type) {
+      case "SET_FIRST_NAME":
+        return {
+          ...state,
+          firstName: action.payload,
+          firstNameError: !/^[a-zA-Z]{3,12}$/.test(action.payload)
+            ? "First name must be 3-12 chars long."
+            : ""
+        };
+      case "SET_LAST_NAME":
+        return {
+          ...state,
+          lastName: action.payload,
+          lastNameError: !/^[a-zA-Z]{3,12}$/.test(action.payload)
+            ? "Last name must be 3-12 chars long."
+            : ""
+        };
+      case "SET_EMAIL":
+        return {
+          ...state,
+          email: action.payload,
+          emailError: !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{3,}$/.test(
+            action.payload
+          )
+            ? "Please enter a valid email"
+            : ""
+        };
+      default:
+        throw new Error();
+    }
+  }
+
+  /* 3. Aplicar el hook para inicializar el estado */
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const handleSubmit = e => {
@@ -64,6 +67,7 @@ function App() {
             placeholder="John..."
             value={state.firstName}
             onChange={e =>
+              /* Usar la funcion dispatch para actualizar el estado */
               dispatch({ type: "SET_FIRST_NAME", payload: e.target.value })
             }
           />
