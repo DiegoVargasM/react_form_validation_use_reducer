e8f196b0945329a8dedcc4595d764109e2b9c444
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-05-05 14:39:01 -0500

    replace useState with useReducer

--- src/App.js
+++ src/App.js
@@ -1,44 +1,55 @@
 import "./App.css";
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialState = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  firstNameError: "",
+  lastNameError: "",
+  emailError: ""
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "SET_FIRST_NAME":
+      return {
+        ...state,
+        firstName: action.payload,
+        firstNameError: !/^[a-zA-Z]{3,12}$/.test(action.payload)
+          ? "First name must be 3-12 chars long."
+          : ""
+      };
+    case "SET_LAST_NAME":
+      return {
+        ...state,
+        lastName: action.payload,
+        lastNameError: !/^[a-zA-Z]{3,12}$/.test(action.payload)
+          ? "Last name must be 3-12 chars long."
+          : ""
+      };
+    case "SET_EMAIL":
+      return {
+        ...state,
+        email: action.payload,
+        emailError: !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{3,}$/.test(
+          action.payload
+        )
+          ? "Please enter a valid email"
+          : ""
+      };
+    default:
+      throw new Error();
+  }
+}
 
 function App() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-
-  /* Add error handling */
-  const [firstNameError, setFirstNameError] = useState("");
-  const [lastNameError, setLastNameError] = useState("");
-  const [emailError, setEmailError] = useState("")
-
-  const checkFirstName = e => {
-    setFirstName(e.target.value);
-    if (!/^[a-zA-Z]{2,12}$/.test(firstName)) {
-      setFirstNameError("Please enter a valid first name")
-    } else {
-      setFirstNameError("")
-    }
-  }
-  const checkLastName = e => {
-    setLastName(e.target.value);
-    if (!/^[a-zA-Z]{2,12}$/.test(lastName)) {
-      setLastNameError("Please enter a valid last name")
-    } else {
-      setLastNameError("")
-    }
-  }
-  const checkEmail = e => {
-    setEmail(e.target.value);
-    if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
-      setEmailError("Please enter a valid email")
-    } else {
-      setEmailError("")
-    }
-  }
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const handleSubmit = e => {
     e.preventDefault();
-  }
+  };
+
   return (
     <div className="App">
       <h1 className="text-center text-3xl font-semibold mt-2 py-4">
@@ -51,11 +62,13 @@ function App() {
             type="text"
             name="firstName"
             placeholder="John..."
-            value={firstName}
-            onChange={checkFirstName}
+            value={state.firstName}
+            onChange={e =>
+              dispatch({ type: "SET_FIRST_NAME", payload: e.target.value })
+            }
           />
         </div>
-        {firstNameError && <p className="error">{firstNameError}</p>}
+        {state.firstNameError && <p>{state.firstNameError}</p>}
 
         <div>
           <label htmlFor="lastName">Last Name:</label>
@@ -63,11 +76,13 @@ function App() {
             type="text"
             name="lastName"
             placeholder="Doe..."
-            value={lastName}
-            onChange={checkLastName}
+            value={state.lastName}
+            onChange={e =>
+              dispatch({ type: "SET_LAST_NAME", payload: e.target.value })
+            }
           />
         </div>
-        {lastNameError && <p className="error">{lastNameError}</p>}
+        {state.lastNameError && <p>{state.lastNameError}</p>}
 
         <div>
           <label htmlFor="email">Email:</label>
@@ -75,11 +90,13 @@ function App() {
             type="text"
             name="email"
             placeholder="john@doe..."
-            value={email}
-            onChange={checkEmail}
+            value={state.email}
+            onChange={e =>
+              dispatch({ type: "SET_EMAIL", payload: e.target.value })
+            }
           />
         </div>
-        {emailError && <p className="error">{emailError}</p>}
+        {state.emailError && <p>{state.emailError}</p>}
         <button type="submit">Sign In</button>
       </form>
     </div>
