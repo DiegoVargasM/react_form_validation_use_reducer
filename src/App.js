92893bd72613234142f0862995f9afe8d7b96159
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-05-05 14:30:51 -0500

    add validation

--- src/App.js
+++ src/App.js
@@ -5,12 +5,46 @@ function App() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
+
+  /* Add error handling */
+  const [firstNameError, setFirstNameError] = useState("");
+  const [lastNameError, setLastNameError] = useState("");
+  const [emailError, setEmailError] = useState("")
+
+  const checkFirstName = e => {
+    setFirstName(e.target.value);
+    if (!/^[a-zA-Z]{2,12}$/.test(firstName)) {
+      setFirstNameError("Please enter a valid first name")
+    } else {
+      setFirstNameError("")
+    }
+  }
+  const checkLastName = e => {
+    setLastName(e.target.value);
+    if (!/^[a-zA-Z]{2,12}$/.test(lastName)) {
+      setLastNameError("Please enter a valid last name")
+    } else {
+      setLastNameError("")
+    }
+  }
+  const checkEmail = e => {
+    setEmail(e.target.value);
+    if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
+      setEmailError("Please enter a valid email")
+    } else {
+      setEmailError("")
+    }
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  }
   return (
     <div className="App">
       <h1 className="text-center text-3xl font-semibold mt-2 py-4">
         Registration Form
       </h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="firstName">First Name:</label>
           <input
@@ -18,9 +52,10 @@ function App() {
             name="firstName"
             placeholder="John..."
             value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
+            onChange={checkFirstName}
           />
         </div>
+        {firstNameError && <p className="error">{firstNameError}</p>}
 
         <div>
           <label htmlFor="lastName">Last Name:</label>
@@ -29,9 +64,10 @@ function App() {
             name="lastName"
             placeholder="Doe..."
             value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
+            onChange={checkLastName}
           />
         </div>
+        {lastNameError && <p className="error">{lastNameError}</p>}
 
         <div>
           <label htmlFor="email">Email:</label>
@@ -40,9 +76,11 @@ function App() {
             name="email"
             placeholder="john@doe..."
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={checkEmail}
           />
         </div>
+        {emailError && <p className="error">{emailError}</p>}
+        <button type="submit">Sign In</button>
       </form>
     </div>
   );
